f7befb6d402afeb593e53f4fabe2fadfe73afadf
Timothy Y Wang <tywang@UCSD.EDU>
2017-04-23 15:28:04 -0700

    starting enemy health

---
+++ Assets/ScriptableObject/EnemyStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "EnemyStats")]
+public class EnemyStats : UnityEngine.ScriptableObject {
+    public int health;
+    public float speed;
+
+}
