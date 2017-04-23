4594a10a40c3f9baabbf418fb46484141d4c5d6e
Timothy Y Wang <tywang@UCSD.EDU>
2017-04-23 13:32:28 -0700

    camerawork

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform p1;
+    public Transform p2;
+
+    public Transform cameraTransform;
+
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+
+    public float zdampTime = 0.15f;
+    private Vector3 zvelocity = Vector3.zero;
+
+    private Camera cam;
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FixedCameraFollowSmooth(cam, p1, p2);
+    }
+
+    // Follow Two Transforms with a Fixed-Orientation Camera
+    public void FixedCameraFollowSmooth(Camera cam, Transform t1, Transform t2)
+    {
+        // How many units should we keep from the players
+        float zoomFactor = 1.5f;
+        float followTimeDelta = 0.15f;
+
+        // Midpoint we're after
+        Vector3 midpoint = (t1.position + t2.position) / 2f;
+
+        // Distance between objects
+        float distance = (t1.position - t2.position).magnitude;
+        distance = Mathf.Clamp(distance, 10.0f , Mathf.Infinity);
+
+        // Move camera a certain distance
+        Vector3 cameraDestination = midpoint - cam.transform.forward * distance * zoomFactor;
+
+        // Adjust ortho size if we're using one of those
+        if (cam.orthographic)
+        {
+            // The camera's forward vector is irrelevant, only this size will matter
+            cam.orthographicSize = distance;
+        }
+        // You specified to use MoveTowards instead of Slerp
+        cam.transform.position = Vector3.Slerp(cam.transform.position, cameraDestination, followTimeDelta);
+
+        // Snap when close enough to prevent annoying slerp behavior
+        if ((cameraDestination - cam.transform.position).magnitude <= 0.05f)
+            cam.transform.position = cameraDestination;
+    }
+}
