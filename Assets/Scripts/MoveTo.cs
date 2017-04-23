bc289ff9983f00d6b784eea520a289a5cea6439c
Timothy Y Wang <tywang@UCSD.EDU>
2017-04-23 14:10:22 -0700

    navmesh

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour {
+
+    public Transform goal;
+    Vector3 destination;
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        destination = goal.position;
+        agent.destination = destination;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(destination, goal.position) > 1.0f)
+        {
+            destination = goal.position;
+            agent.destination = destination;
+        }
+    }
+}
