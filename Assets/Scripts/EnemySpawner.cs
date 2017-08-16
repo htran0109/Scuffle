e1402ab504149ecca47437513fac4cef6c901143
hqtran <hqtran@ucsd.edu>
2017-08-15 22:38:10 -0600

    Added a spawner
    Spawner for enemies exists for level generation. TODO make level, add
    second character

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+
+    public Vector3[] spawnLocations;
+    public Object enemyPrefab;
+    private bool spawned = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (!spawned)
+        {
+            for (int i = 0; i < spawnLocations.Length; i++)
+            {
+                Instantiate(enemyPrefab, spawnLocations[i], Quaternion.identity);
+            }
+            DestroyObject(this.gameObject);
+            Destroy(this);
+            spawned = true;
+        }
+
+    }
+}
+
