a18878ea41a03febed92043d7b94ead9b7f9ff47
hqtran <hqtran@ucsd.edu>
2017-09-19 03:32:09 -0700

    Walls change color, Needs change of material for transparency

---
+++ Assets/Scripts/PlayerWallCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallCheck : MonoBehaviour {
+    public GameObject mainCamera;
+    public bool foundWall = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+
+        foundWall = false;
+        Debug.DrawRay(this.gameObject.transform.position, mainCamera.gameObject.transform.position, Color.red,0.5f);
+        if (Physics.Linecast(this.gameObject.transform.position, mainCamera.gameObject.transform.position, out hit))
+        {
+            Transform objectHit = hit.transform;
+            if(objectHit.gameObject.tag == "Wall")
+            {
+                Debug.Log("Make wall transparent");
+                objectHit.gameObject.GetComponent<Wall>().makeTransparentCounter = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+
+    public int makeTransparentCounter = 3;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(makeTransparentCounter <= 2)
+        {
+            Debug.Log("Transparent wall here");
+            this.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, .2f);
+        }
+        else
+        {
+            this.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 0.2f, 0.2f, 1f);
+        }
+
+        makeTransparentCounter++;
+    }
+
+}
